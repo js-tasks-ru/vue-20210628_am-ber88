38e14634247bfa7a9532f00c1785d3d51d93042e
Kristina Prokopova <am_ber88@mail.ru>
2021-07-24 20:00:35 +0300

    Solve calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,35 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+
+const RootComponentOptions = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      deist: '',
+    };
+  },
+
+  computed: {
+    setRes() {
+      if (this.deist === 'sum') {
+        return this.num1 + this.num2;
+      } else if (this.deist === 'subtract') {
+        return this.num1 - this.num2;
+      } else if (this.deist === 'multiply') {
+        return this.num1 * this.num2;
+      } else if (this.deist === 'divide') {
+        return this.num1 / this.num2;
+      } else {
+        return 0;
+      }
+    },
+  },
+});
+
+const app = createApp(RootComponentOptions);
+const vm = app.mount('#app');
