f1111a6634c1b50ebf0b0eab84ee23f9a2a558e8
Kristina Prokopova <am_ber88@mail.ru>
2021-08-07 10:43:01 +0300

    Solve MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,53 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      require: true,
+    },
+    place: {
+      type: String,
+      require: true,
+    },
+    date: {
+      type: Number,
+      require: true,
+    },
+  },
+
+  computed: {
+    full_date() {
+      return new Date(this.date).toLocaleString('eng', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+    d_time() {
+      let d = new Date(this.date);
+      let month = '' + (d.getMonth() + 1);
+      let day = '' + d.getDate();
+      let year = d.getFullYear();
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+      return [year, month, day].join('-');
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="d_time">{{ full_date }}</time>
       </li>
     </ul>`,
 });
